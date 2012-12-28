acad9d954afca71163faac26f175c61d671e414b
Arthur Neves <arthurnn@gmail.com>
2012-12-28 17:11:14 -0500

    moving touch method to its respective module

--- lib/mongoid/relations/touchable.rb
+++ lib/mongoid/relations/touchable.rb
@@ -9,6 +9,34 @@ module Mongoid
         self.touchables = []
       end
 
+      # Touch the document, in effect updating its updated_at timestamp and
+      # optionally the provided field to the current time. If any belongs_to
+      # relations exist with a touch option, they will be updated as well.
+      #
+      # @example Update the updated_at timestamp.
+      #   document.touch
+      #
+      # @example Update the updated_at and provided timestamps.
+      #   document.touch(:audited)
+      #
+      # @note This will not autobuild relations if those options are set.
+      #
+      # @param [ Symbol ] field The name of an additional field to update.
+      #
+      # @return [ true ] true.
+      #
+      # @since 3.0.0
+      def touch(field = nil)
+        current = Time.now
+        write_attribute(:updated_at, current) if fields["updated_at"]
+        write_attribute(field, current) if field
+        _root.collection.find(atomic_selector).update(touch_atomic_updates(field))
+        without_autobuild do
+          self.touchables.each { |name| send(name).try(:touch) }
+        end
+        move_changes and true
+      end
+
       module ClassMethods
 
         # Add the metadata to the touchable relations if the touch option was
