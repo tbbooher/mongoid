b25d4bfa638be9497ffb935a8573f922f05004e3
Arthur Neves <arthurnn@gmail.com>
2012-12-31 12:58:58 -0500

    moving Unset custom code to Opration module

--- lib/mongoid/persistence/atomic/unset.rb
+++ lib/mongoid/persistence/atomic/unset.rb
@@ -7,44 +7,6 @@ module Mongoid
       class Unset
         include Operation
 
-        attr_accessor :fields
-
-        def initialize(document, fields, value, options = {})
-          @document, @value, @options = document, value, options
-
-          @fields = Array.wrap(fields).collect do |field|
-            document.database_field_name(field.to_s)
-          end
-
-          self.class.send(:define_method, :field) do
-            @fields.first
-          end if @fields.length == 1
-
-        end
-
-        def operation(modifier)
-          hash = Hash[fields.collect do |field|
-            [path(field), cast_value]
-          end]
-          { modifier => hash }
-        end
-
-        def path(field = field)
-          position = document.atomic_position
-          position.blank? ? field : "#{position}.#{field}"
-        end
-
-        def execute(name)
-          unless document.new_record?
-            collection.find(document.atomic_selector).update(operation(name))
-            if fields.length > 1
-              document.remove_change(fields)
-            else
-              document.remove_change(field)
-            end
-          end
-        end
-
         # Sends the atomic $unset operation to the database.
         #
         # @example Persist the new values.
