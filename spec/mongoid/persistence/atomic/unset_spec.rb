6e4382be5415058b33b3c0bcb89a63f2823ddeff
Arthur Neves <arthurnn@gmail.com>
2013-01-04 22:17:52 -0500

    adding tests for multiple arguments on unset method

--- spec/mongoid/persistence/atomic/unset_spec.rb
+++ spec/mongoid/persistence/atomic/unset_spec.rb
@@ -27,34 +27,32 @@ describe Mongoid::Persistence::Atomic::Unset do
       end
     end
 
-    context "when unsetting multiple fields" do
 
-      let(:person) do
-        Person.create(age: 100, score: 2)
-      end
+    [[:age,:score, {safe: true}], [:age, :score], [[:age,:score]]].each do |args|
 
-      let!(:removed) do
-        person.unset(:age, :score)
-      end
+      context "when unsetting multiple fields using #{args}" do
 
-      it "removes age field" do
-        person.age.should be_nil
-      end
-
-      it "removes score field" do
-        person.score.should be_nil
-      end
-
-      it "resets the age dirty attribute" do
-        person.changes["age"].should be_nil
-      end
-
-      it "resets the score dirty attribute" do
-        person.changes["score"].should be_nil
-      end
-
-      it "returns nil" do
-        removed.should be_nil
+        let(:person) do
+          Person.create(age: 100, score: 2)
+        end
+        let!(:removed) do
+          person.unset *(args)
+        end
+        it "removes age field" do
+          person.age.should be_nil
+        end
+        it "removes score field" do
+          person.score.should be_nil
+        end
+        it "resets the age dirty attribute" do
+          person.changes["age"].should be_nil
+        end
+        it "resets the score dirty attribute" do
+          person.changes["score"].should be_nil
+        end
+        it "returns nil" do
+          removed.should be_nil
+        end
       end
     end
 
